6518749d79161cb58e90def631218c662dc206a7
Ang <naiadelle@freenet.de>
2019-05-06 13:42:19 +0200

    objects exercise 1

---
+++ githup_exercises/javascript/objects/objects_exercise.js
@@ -0,0 +1,99 @@
+// Objects  
+
+/* 1. Write a program to list the properties of an object. 
+E.g.
+const student = { 
+firstName: "John", 
+lastName: "Smith", 
+class: 12 };
+Expected Output: firstName, lastName, class */
+
+let client = {
+    userName: "Lila19",
+    plan: "Gold",
+    contract: "2 years",
+}
+console.log(client); // { userName: 'Lila19', plan: 'Gold', contract: '2 years' }
+
+
+/* 2. Create a person object. Include the person's first and last name, age, job, city etc. 
+Then print text by retrieving data from the object e.g. "John Smith is a 41 year old engineer 
+living in France". */ 
+
+let person = {
+    firstName: "Lila",
+    lastName: "Moon",
+    age: "23",
+    job: "Dancer",
+    city: "Berlin",
+}
+console.log(`${person.firstName} ${person.lastName} is a ${person.age} year old ${person.job} living in ${person.city}.`);
+// Lila Moon is a 23 year old Dancer living in Berlin.
+
+/* 3. Write a JavaScript program to delete the "class" property (or last property) from the previous 
+object. */
+
+delete person.city;
+console.log(person); // { firstName: 'Lila', lastName: 'Moon', age: '23', job: 'Dancer' }
+
+
+/* 4. Write a program to get the length of a JavaScript object. */
+
+console.log(Object.keys (person).length); // 4
+
+
+/* 5. Write a program to display the reading status (i.e. display book name, author name and reading 
+status) of the following books.
+const library = [ 
+   {
+       author: 'J.K. Rowling',
+       title: 'Harry Potter and the Chamber of Secrets',
+       readingStatus: true
+   },
+   {
+       author: 'Homer',
+       title: 'The Odyssey',
+       readingStatus: true
+   },
+   {
+       author: 'Harper Lee',
+       title:  'To Kill a Mockingbird', 
+       readingStatus: false
+   }];
+E.g. Output: 
+Already read Harry Potter and the Chamber of Secrets by J.K. Rowling
+Already read The Odyssey by Homer
+You still need to read To Kill a Mockingbird by Harper Lee */
+
+let library = [ 
+    {
+        author: 'J.K. Rowling',
+        title: 'Harry Potter and the Chamber of Secrets',
+        readingStatus: true,
+    },
+    {
+        author: 'Homer',
+        title: 'The Odyssey',
+        readingStatus: true,
+    },
+    {
+        author: 'Harper Lee',
+        title:  'To Kill a Mockingbird', 
+        readingStatus: false,
+    }
+];
+
+for (let i = 0; i < library.length; i++) 
+   {
+    let book = `${library[i].title} by ${library[i].author}`;
+    if (library[i].readingStatus) {
+      console.log(`Already read ${book}`);
+    } else
+    {
+     console.log(`You still need to read ${book}`);
+    }
+   }
+    /* Already read Harry Potter and the Chamber of Secrets by J.K. Rowling
+    Already read The Odyssey by Homer
+    You still need to read To Kill a Mockingbird by Harper Lee
+     */
