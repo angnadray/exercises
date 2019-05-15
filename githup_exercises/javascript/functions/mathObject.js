8fbb122ab6a7346fb4c049d7368c33194777de56
Ang <naiadelle@freenet.de>
2019-05-15 13:30:18 +0200

    exercises completed

--- githup_exercises/javascript/functions/mathObject.js
+++ githup_exercises/javascript/functions/mathObject.js
@@ -12,6 +12,27 @@ console.log(randomNum(2, 89)); // 25
 Create a function that takes an array as an argument and returns the number with 
 the lowest value. */ 
 
+function mathMin() {
+    let numArray = [654, -3, 0, 6577, -2, 0.44, 40, -1, 100];
+    let smallNum;
+
+    for (let i = 0; i < numArray.length; i++) {
+        smallNum = numArray[i];
+
+        for (let j = 0; j < numArray.length; j++) {
+            // these 2 loops compare each number with all the other numbers in the array
+            smallNum = Math.min(smallNum, numArray[j]);
+
+        }
+        return smallNum;
+
+    }
+}
+
+console.log(mathMin()); // -3
+
+//_________________________________________________
+
 function lowNum (arr1) {
     let minNum = arr1.reduce((a, b) => Math.min(a, b))
     console.log(minNum);
@@ -31,6 +52,27 @@ console.log(powOf(2, 3));
 Create a function that takes an array as an argument and returns the highest number 
 in that array. */ 
 
+function mathMax() {
+    let numArray2 = [654, -3, 0, 6577, -2, 0.44, 40, -1, 100];
+    let maxNum;
+
+    for (let i = 0; i < numArray2.length; i++) {
+        maxNum = numArray2[i];
+
+        for (let j = 0; j < numArray2.length; j++) {
+            // these 2 loops compare each number with all the other numbers in the array
+            maxNum = Math.max(maxNum, numArray2[j]);
+
+        }
+        return maxNum;
+
+    }
+}
+
+console.log(mathMax()); // -3
+
+//__________________________________________________
+
 function maxNum (arr2) {
     let maxNum = arr2.reduce((a, b) => Math.max(a, b))
     console.log(maxNum);
@@ -44,7 +86,10 @@ Examples:
 * myPi(4) ➞ 3.1416 //4 decimal places
 * myPi(15) ➞ 3.141592653589793 // 15 decimal places */
 
-
+function myPi(decPlaces) {
+    return Math.PI.toFixed(decPlaces);
+}
+console.log(myPi(5));
 
 /* **Bonus** 
 #### Random Name 
