d9e5ce51c917ddbc7909b92f2bc0c973bdcc1109
Ang <naiadelle@freenet.de>
2019-05-16 15:39:21 +0200

    answer to question 2

--- githup_exercises/javascript/functions/dateObject_exercise.js
+++ githup_exercises/javascript/functions/dateObject_exercise.js
@@ -19,7 +19,17 @@ date();
 /* 2. Date Formats
 Create a function to display the current date in the following formats dd-mm-yyyy, dd/mm/yyyy. */
 
+function dateForm() {
+    let dateForm = new Date();
+    let months2 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+    let getMonthName2 = months2[dateForm.getMonth()];
 
+    let dateFormResult = `${dateForm.getDate()}-${getMonthName2}-${dateForm.getFullYear()}`;
+    console.log(dateFormResult);
+    let dateFormResult2 = `${dateForm.getDate()}/${getMonthName2}/${dateForm.getFullYear()}`;
+    console.log(dateFormResult2);
+}
+dateForm();
 
 /* 3. Is New Year's day a Sunday?
 Create a function to check whether the 1st of January is a Sunday 
